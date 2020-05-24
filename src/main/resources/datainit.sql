2e68686aae615456443b1538649f0f76324af0da
Pier-Hugo <pierhugocarpentier36@gmail.com>
2020-05-24 10:30:17 +0200

    TODO : les champs de register qui s'effacent pas au moment de s'enregistrer

--- src/main/resources/datainit.sql
+++ src/main/resources/datainit.sql
@@ -10,7 +10,7 @@ create table member
 create table comment
 (
     author       varchar(255) not null primary key,
-    comment      varchar(255),
+    comment      varchar(500),
     comment_date varchar(20),
     likes        bigint default 0
 ) engine = InnoDB;
@@ -21,3 +21,12 @@ VALUES ('member0', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d15
 INSERT INTO member
 VALUES ('member1', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'member1', 'member1', 'Tours');
 -- password : password
+
+INSERT INTO comment
+VALUES ('author0',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget eleifend urna. Pellentesque ex eros, condimentum ut luctus eget, mattis eget nisl. Morbi elementum sit amet nisl a venenatis. Nam eu eros sit amet nulla suscipit rutrum. Mauris ornare eu quam vel sagittis. Maecenas eu justo neque. Sed mattis mi eu erat tincidunt molestie.',
+        now(), 10);
+INSERT INTO comment
+VALUES ('author1',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget eleifend urna. Pellentesque ex eros, condimentum ut luctus eget, mattis eget nisl. Morbi elementum sit amet nisl a venenatis. Nam eu eros sit amet nulla suscipit rutrum. Mauris ornare eu quam vel sagittis. Maecenas eu justo neque. Sed mattis mi eu erat tincidunt molestie.',
+        now(), 5);
